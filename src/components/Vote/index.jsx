69fbcdaf95fc37ca784c5deacb172df3e1a8e802
tranbaolam <tranbaolam26902@gmail.com>
2022-09-20 18:20:30 +0700

    Add global states & Login

--- src/components/Vote/index.jsx
+++ src/components/Vote/index.jsx
@@ -1,48 +1,58 @@
 import { useState } from 'react';
 import classNames from 'classnames/bind';
 
+import { useStore, actions } from '../../store';
 import styles from './Vote.module.scss';
 import icons from '../../assets/icons';
 
 const cx = classNames.bind(styles);
 
 function Vote({ voted, action, children }) {
+    const [states, dispatch] = useStore();
+    const { token } = states;
     const [voteUpIcon, setVoteUpIcon] = useState(icons.voteUp);
     const [voteDownIcon, setVoteDownIcon] = useState(icons.voteDown);
-    
     const handleVoteUp = () => {
-        if (!voted.up) {
-            setVoteUpIcon(icons.voteUpTrue);
-            if (voted.down) {
-                action.setDown(!voted.down);
-                setVoteDownIcon(icons.voteDown);
-            }
-        } else {
-            setVoteUpIcon(icons.voteUp);
-        }
-        action.setUp(!voted.up);
-    }
-    const handleVoteDown = () => {
-        if (!voted.down) {
-            setVoteDownIcon(icons.voteDownTrue);
-            if (voted.up) {
-                action.setUp(!voted.up);
+        if (token != '') {
+            if (!voted.up) {
+                setVoteUpIcon(icons.voteUpTrue);
+                if (voted.down) {
+                    action.setDown(!voted.down);
+                    setVoteDownIcon(icons.voteDown);
+                }
+            } else {
                 setVoteUpIcon(icons.voteUp);
             }
+            action.setUp(!voted.up);
         } else {
-            setVoteDownIcon(icons.voteDown);
+            dispatch(actions.setShowLogin(true));
         }
-        action.setDown(!voted.down);
-    }
+    };
+    const handleVoteDown = () => {
+        if (token != '') {
+            if (!voted.down) {
+                setVoteDownIcon(icons.voteDownTrue);
+                if (voted.up) {
+                    action.setUp(!voted.up);
+                    setVoteUpIcon(icons.voteUp);
+                }
+            } else {
+                setVoteDownIcon(icons.voteDown);
+            }
+            action.setDown(!voted.down);
+        } else {
+            dispatch(actions.setShowLogin(true));
+        }
+    };
 
     return (
         <div className={cx('wrapper')}>
-            <button>
-                <img src={voteUpIcon} alt='icon-vote-up' onClick={handleVoteUp} />
+            <button onClick={handleVoteUp}>
+                <img src={voteUpIcon} alt='icon-vote-up' />
             </button>
-            <span className={cx('vote', {'vote-up': voted.up, 'vote-down': voted.down})}>{children}</span>
-            <button>
-                <img src={voteDownIcon} alt='icon-vote-down' onClick={handleVoteDown} />
+            <span className={cx('vote', { 'vote-up': voted.up, 'vote-down': voted.down })}>{children}</span>
+            <button onClick={handleVoteDown}>
+                <img src={voteDownIcon} alt='icon-vote-down' />
             </button>
         </div>
     );
